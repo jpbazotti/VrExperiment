5ad185b3c661d72357a3a910c1f7238e55e31f8b
jpbazotti <62719223+jpbazotti@users.noreply.github.com>
2021-12-11 21:59:55 -0300

    body final fix

--- Assets/cameraFollow.cs
+++ Assets/cameraFollow.cs
@@ -16,10 +16,9 @@ public class cameraFollow : MonoBehaviour
     void Update()
     {
         Debug.Log(camPos.rotation.x);
-        Vector3 newVec = new Vector3(camPos.position.x, camPos.position.y - 0.75f, camPos.position.z);
-        Vector3 newRotation = new Vector3(0, camPos.eulerAngles.y, 0);
-        transform.eulerAngles = newRotation;
-        Vector3 modifier = Vector3.ProjectOnPlane(camPos.forward, Vector3.up).normalized;
+        Vector3 newVec = new Vector3(camPos.position.x, camPos.position.y - 0.65f, camPos.position.z);
+        transform.LookAt(newVec);
+        Vector3 modifier = Vector3.ProjectOnPlane(camPos.forward+(camPos.up*1.5f), Vector3.up).normalized;
         transform.position = newVec-(modifier*0.1f);
         Debug.DrawLine(camPos.position,camPos.position + Vector3.ProjectOnPlane(camPos.forward, Vector3.up).normalized, Color.blue);
        
